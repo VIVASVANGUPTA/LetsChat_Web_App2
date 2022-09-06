ea6f075be09743eb7b507b6eb4791c15b9354f2b
VIVASVANGUPTA <104221230+VIVASVANGUPTA@users.noreply.github.com>
2022-09-06 09:58:05 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBdnXXhoLwmTA8IGcmrcO5SqnQLSz7i7WE",
+    authDomain: "letchat-web-app.firebaseapp.com",
+    projectId: "letchat-web-app",
+    storageBucket: "letchat-web-app.appspot.com",
+    messagingSenderId: "57570877349",
+    appId: "1:57570877349:web:0c56272f39e82a731a924c"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
